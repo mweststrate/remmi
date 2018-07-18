07613e121770333d88a22525eddf29ec0cd88927
Michel Weststrate <mweststrate@gmail.com>
2018-07-18 20:36:30 +0200

    optimized tracker

--- src/Tracker.ts
+++ src/Tracker.ts
@@ -1,4 +1,4 @@
-import { Lens, Merge, shallowEqual, noop, Disposer } from "./internal";
+import { merge, Lens, Merge, shallowEqual, noop, Disposer } from "./internal";
 
 // TODO: use a global
 let readListener: undefined | ((lens: Lens) => void)
@@ -18,17 +18,24 @@ export class Tracker {
     public track<T>(fn: () => T): T {
         const dependencies = new Set<Lens>()
         const prevListener = readListener
-        readListener = dependencies.add.bind(dependencies)
+        readListener = lens => {
+            console.trace("saw dep", lens)
+            dependencies.add(lens)
+        }
         try {
             return fn()
         } finally {
             const newDeps = Array.from(dependencies)
             if (!shallowEqual(newDeps, this.merge.bases)) { // TODO: still needed with merge caching?
-                const { disposeMerge } = this
-                // optimization; don't create merge if only one dep
-                this.merge = new Merge(newDeps as any) // TODO: fix typings
-                this.disposeMerge = this.merge.subscribe(this.onInvalidate)
-                disposeMerge()
+
+            //     // don't create merge if only one dep
+            const nextMerge = newDeps.length === 1 ? newDeps[0] : merge.apply(undefined, newDeps as any) // TODO: fix typings
+                if (nextMerge !== this.merge) {
+                    const { disposeMerge } = this
+                    this.merge = nextMerge
+                    this.disposeMerge = this.merge.subscribe(this.onInvalidate)
+                    disposeMerge()
+                }
             }
             readListener = prevListener
         }
@@ -39,7 +46,7 @@ export class Tracker {
     }
 
     public getDependencies(): Lens[] {
-        return this.merge.bases
+        return this.merge instanceof Merge ? this.merge.bases : [this.merge]
     }
 }
 
